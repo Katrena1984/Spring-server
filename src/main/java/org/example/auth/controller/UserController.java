9e72803ba57eb6bfa52d1d0aa34130e46b3d26c6
Katrena1984 <grishenko2007@yandex.ru>
2026-05-22 00:52:45 +0700

    Update UserController.java

--- src/main/java/org/example/auth/controller/UserController.java
+++ src/main/java/org/example/auth/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.example.auth.service.UserService;
+import org.springframework.security.core.Authentication;
 
 @RequiredArgsConstructor
 @RestController
