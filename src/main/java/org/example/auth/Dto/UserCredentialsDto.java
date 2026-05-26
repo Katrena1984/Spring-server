ccdc2d522508c58e7d02b2c6c517dc9d03c331db
Katrena1984 <grishenko2007@yandex.ru>
2026-05-26 22:38:46 +0700

    Update UserCredentialsDto.java

--- src/main/java/org/example/auth/Dto/UserCredentialsDto.java
+++ src/main/java/org/example/auth/Dto/UserCredentialsDto.java
@@ -1,7 +1,11 @@
 package org.example.auth.Dto;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserCredentialsDto {
     private String email;
     private String password;
