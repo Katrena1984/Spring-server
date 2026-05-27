fc34f66ffb7adc909c69f1334c2b30fe3a8a95e4
Katrena1984 <grishenko2007@yandex.ru>
2026-05-28 01:55:11 +0700

    Update AuthApplication.java

--- src/main/java/org/example/auth/AuthApplication.java
+++ src/main/java/org/example/auth/AuthApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "org.example.auth") 
 public class AuthApplication {
 
     public static void main(String[] args) {
