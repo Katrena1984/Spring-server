01bc4e2e778377a91e7f43b70d1da854da56faff
Katrena1984 <grishenko2007@yandex.ru>
2026-06-03 00:33:27 +0700

    Delete src/main/resources/data.sql

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO registration_codes (code, role, is_used) 
-VALUES 
-('ADMIN-KEY-2024', 'ADMIN', false),
-('EDITOR-KEY-2024', 'EDITOR', false),
-('VIEWER-KEY-2024', 'VIEWER', false)
-ON CONFLICT (code) DO NOTHING;
