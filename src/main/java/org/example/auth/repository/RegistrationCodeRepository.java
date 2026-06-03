39c146474fd860c2f88bd14cfe8349a411ffc62d
Katrena1984 <grishenko2007@yandex.ru>
2026-06-03 20:16:43 +0700

    Update RegistrationCodeRepository.java

--- src/main/java/org/example/auth/repository/RegistrationCodeRepository.java
+++ src/main/java/org/example/auth/repository/RegistrationCodeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RegistrationCodeRepository extends JpaRepository<RegistrationCode, Long> {
-    Optional<RegistrationCode> findByCodeAndIsUsedFalse(String code);
+    Optional<RegistrationCode> findByCode(String code);
 }
